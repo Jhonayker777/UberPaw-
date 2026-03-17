6d92681bdd5e2a38935595f538225225be6a6653
Jhonayker777 <jhon117.hello@gmail.com>
2026-03-17 10:48:14 -0500

    feat: Se agrago una nueva clase (Macotas)

---
+++ Mascotas.py
@@ -0,0 +1,4 @@
+def Datos: 
+	nombreMascota=input("Por favor ingrese el nombre de la mascota")
+	tamañoMascota=int(input("Ingrese el tamaño de la mascota ena cm")
+	Transporte=input("Se requiere jaula para trasportar la mascota?")
