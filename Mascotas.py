e1b6545b41cebc37182165cf906534eafd7a4f29
Jhonayker777 <jhon117.hello@gmail.com>
2026-03-17 11:21:54 -0500

    Feat: La clase Mascota se actualizo

--- Mascotas.py
+++ Mascotas.py
@@ -1,4 +1,4 @@
-def Datos: 
+def Datos(): 
 	nombreMascota=input("Por favor ingrese el nombre de la mascota")
-	tamañoMascota=int(input("Ingrese el tamaño de la mascota ena cm")
+	tamañoMascota=int(input("Ingrese el tamaño de la mascota en cm"))
 	Transporte=input("Se requiere jaula para trasportar la mascota?")
