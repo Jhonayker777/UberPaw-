dd7ce7f5049de5f78f086eff0aec28991624d6e4
Jhonayker777 <jhon117.hello@gmail.com>
2026-03-17 13:00:54 -0500

    feat: Se completo la funcionlidad Datos de mascotas

--- Mascotas.py
+++ Mascotas.py
@@ -1,4 +1,31 @@
+from validaciones import validarNombre, validarDecimal, validarEntero
+from gestionjson import generar_id, cargar, guardar
+ArchivoMascotas = "mascotas.json"
+
 def Datos(): 
-	nombreMascota=input("Por favor ingrese el nombre de la mascota")
-	tamañoMascota=int(input("Ingrese el tamaño de la mascota en cm"))
-	Transporte=input("Se requiere jaula para trasportar la mascota?")
+	mascotas=cargar(ArchivoMascotas)
+
+	nombreMascota=validarNombre("Por favor ingrese el nombre de la mascota")
+	Edad = validarEntero("Ingrese la edad de su mascota")
+	tamañoMascota=validarDecimal(input("Ingrese el tamaño de la mascota en cm"))
+	Transporte=validarNombre("Se requiere jaula para trasportar la mascota?")
+	Comportamiento= validarNombre("Describa a su mascota (Juguetón, Cariñoso, Protector, Independiente, Tímido): ")
+	Raza = validarNombre("Por favor ingrese la raza de su mascota: ")
+	Vacunas = validarNombre("Que vacunas tiene su mascota")
+
+	
+	
+
+	nueva_mascota={
+		"id": generar_id(mascotas),
+		"nombre": nombreMascota,
+		"Edad": Edad,
+		"tamaño": tamañoMascota,
+		"transporte": Transporte,
+		"comportamiento": Comportamiento,
+		"raza": Raza,
+		"vacunas": Vacunas
+	}
+	mascotas.append(nueva_mascota)
+	guardar(ArchivoMascotas,mascotas)
+	print("MASCOTA GUARDADA")
